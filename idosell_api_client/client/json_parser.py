0d4c260ff0112c8e2cdbc3ffac61d5caf91bb4dd
Marcin <marcinszymanek93@gmail.com>
2023-11-18 21:17:29 +0100

    Add JSONParser class to handle JSON parsing and retrieval of product details

---
+++ idosell_api_client/client/json_parser.py
@@ -0,0 +1,27 @@
+import json
+
+
+class JSONParser:
+    def __init__(self, json_data):
+        self.data = json.load(json_data)
+
+    def get_product_details(self, product_id):
+        for product in self.data.get("results", []):
+            if product.get("productId") == product_id:
+                return {
+                    "id": product.get("productId"),
+                    "name": product.get("productName"),
+                    "price": product.get("productRetailPrice"),
+                    "producer": product.get("producerName"),
+                    "category": product.get("categoryName"),
+                }
+        return None
+
+    def get_all_product_ids(self):
+        return [product.get("productId") for product in self.data.get("results", [])]
+
+    def get_stock_info(self, product_id):
+        for product in self.data.get("results", []):
+            if product.get("productId") == product_id:
+                return product.get("productStocksData", [])
+        return None
