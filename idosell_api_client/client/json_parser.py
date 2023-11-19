f6c872696c1e6c0e7ab444708f677919d77615f7
Marcin <marcinszymanek93@gmail.com>
2023-11-19 12:14:03 +0100

    Add error handling in JSONParser class

--- idosell_api_client/client/json_parser.py
+++ idosell_api_client/client/json_parser.py
@@ -1,9 +1,19 @@
 import json
+from exceptions.api_exceptions import APIError
 
 
 class JSONParser:
     def __init__(self, json_data):
         self.data = json_data
+        self.check_for_errors()
+
+    def check_for_errors(self):
+        error_info = self.data.get("errors", {})
+        fault_code = error_info.get("faultCode")
+        fault_string = error_info.get("faultString")
+
+        if fault_code != 0 or fault_string:
+            raise APIError(fault_string, fault_code)
 
     def get_all_product_ids(self):
         return [product.get("productId") for product in self.data.get("results", [])]
