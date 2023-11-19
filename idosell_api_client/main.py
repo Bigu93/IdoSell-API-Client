1e4210ca246bcb20a1a8239b0e08ed19688cb0b3
Marcin <marcinszymanek93@gmail.com>
2023-11-19 12:18:07 +0100

    Remove unused code and update imports

--- idosell_api_client/main.py
+++ idosell_api_client/main.py
@@ -1,7 +1,7 @@
 import json
 from client.product_client import ProductClient
 from client.sku_client import SKUClient
-from client.json_parser import JSONParser
+from client.json_product_parser import JSONParser
 from config.settings import (
     BASE_URL,
     CLIENT_SECRET,
@@ -14,11 +14,11 @@ def main():
     auth = Auth(CLIENT_USERNAME, CLIENT_SECRET, BASE_URL)
     token = auth.get_token()
 
-    product_client = ProductClient(BASE_URL, token)
+    # product_client = ProductClient(BASE_URL, token)
     sku_client = SKUClient(BASE_URL, token)
 
     product_json = sku_client.get_product("28801")
-    product_wrapper = JSONParser(product_json)
+    # product_wrapper = JSONParser(product_json)
 
     product_info = product_wrapper.all_data("")
     print(product_info)
