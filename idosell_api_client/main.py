75d318f07fc7414cbb62da80c6d133dd8eacd899
Marcin <marcinszymanek93@gmail.com>
2023-11-19 12:38:08 +0100

    Add SKUJSONParser import and uncomment SKUClient instantiation

--- idosell_api_client/main.py
+++ idosell_api_client/main.py
@@ -2,6 +2,7 @@ import json
 from client.product_client import ProductClient
 from client.sku_client import SKUClient
 from parsers.product_json import ProductJSONParser
+from parsers.sku_json import SKUJSONParser
 from config.settings import (
     BASE_URL,
     CLIENT_SECRET,
@@ -14,10 +15,10 @@ def main():
     auth = Auth(CLIENT_USERNAME, CLIENT_SECRET, BASE_URL)
     token = auth.get_token()
 
-    product_client = ProductClient(BASE_URL, token)
-    # sku_client = SKUClient(BASE_URL, token)
+    # product_client = ProductClient(BASE_URL, token)
+    sku_client = SKUClient(BASE_URL, token)
 
-    product_json = product_client.get_product("28801-B")
+    product_json = sku_client.get_product("28801")
     product_wrapper = ProductJSONParser(product_json)
 
     product_info = product_wrapper.parse("spa")
---
+++ idosell_api_client/parsers/sku_json.py
@@ -0,0 +1,11 @@
+from .base_json import BaseJSONParser
+
+
+class SKUJSONParser(BaseJSONParser):
+    def parse(self):
+        # Implement SKU-specific parsing logic
+        pass
+
+    def get_data(self):
+        # Implement SKU-specific data retrieval logic
+        pass
