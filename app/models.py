fbfd64aeb78f982b41c3cffe066764b99bfb685c
Marcin <marcinszymanek93@gmail.com>
2023-11-24 08:45:50 +0100

    Delete unnecessary files and code

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from . import db
-from flask_login import UserMixin
-
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.Column(db.String(40), nullable=False)
